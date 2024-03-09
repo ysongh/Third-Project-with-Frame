963a87dd1dfedd2e4eeebc759bcc74581a55617e
You Song Hou <ysongweb3@gmail.com>
2024-03-08 22:30:20 -0500

    feat: add a text input

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import { serve } from '@hono/node-server'
-import { Button, Frog } from 'frog'
+import { Button, TextInput, Frog } from 'frog'
  
 export const app = new Frog()
  
 app.frame('/', (c) => {
-  const { buttonValue, status } = c
+  const { inputText, buttonValue, status } = c
   return c.res({
     image: (
       <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
         {status === 'initial' ? (
           'Select your fruit!'
         ) : (
-          `Selected: ${buttonValue}`
+          `Selected: ${inputText || buttonValue}`
         )}
       </div>
     ),
     intents: [
+      <TextInput placeholder="Enter your fruit..." />,
       <Button value="apple">Apple</Button>,
       <Button value="banana">Banana</Button>,
       <Button value="mango">Mango</Button>,
